2da244d7f3c422942d91430d38e51d5e612e8a35
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2022-10-11 16:18:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+x = 'hello world storm house apple microphone cammel'
+import time
+print("hello world storm house apple microphone cammel")
+def time_convert(sec):
+  mins = sec // 60
+  sec = sec % 60
+  hours = mins // 60
+  mins = mins % 60
+  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+input("Press Enter to start")
+start_time = time.time()
+
+def wpm():
+    slenth = len(x)
+    numword = slenth / 4.7
+    wordspm = numword / time_lapsed
+ 
+    print("Your WPM is: ", wpm)
+
+def start():
+    if input() == x:
+        end_time = time.time()
+        global time_lapsed
+        time_lapsed = end_time - start_time
+        time_convert(time_lapsed)
+        print(time_lapsed)
+        wpm()
+    else:
+        print("you fucked up")
+        start()
+
+start()
