e4bf9593f30f299d32375c2e7f836fdec47ca9f8
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2022-10-12 09:37:53 +0200

    Janky text input

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time 
 import random
 
+
 promptlen = int(input("Enter number of words you want in your prompt \n"))
 
 word = []
 
-
+#creates list of words
 with open("words.txt", "r") as file:
     allText = file.read()
     words = list(map(str, allText.split()))
@@ -13,8 +14,8 @@ with open("words.txt", "r") as file:
       word.append(random.choice(words))
     prompt = ' '.join(map(str,word))
     print(prompt)
-    
 
+#converts time to minutes and seconds    
 def time_convert(sec):
   mins = sec // 60
   sec = sec % 60
@@ -22,9 +23,7 @@ def time_convert(sec):
   mins = mins % 60
   print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
 
-input("Press Enter to start")
-start_time = time.time()
-
+#calculates words per minute
 def wpm():
     slenth = len(prompt)
     numword = slenth / 4.7
@@ -32,16 +31,19 @@ def wpm():
     wordspm = str(wordspm)
     print("Your WPM is: ", wordspm)
 
+#Starts the timer and calculate the elapsed time
 def start():
-    if input() == prompt:
-        end_time = time.time()
-        global time_lapsed
-        time_lapsed = end_time - start_time
-        time_convert(time_lapsed)
-        print(time_lapsed)
-        wpm()
-    else:
-        print("you fucked up")
-        start()
+  input("Press Enter to start")
+  start_time = time.time()
+  if input() == prompt:
+      end_time = time.time()
+      global time_lapsed
+      time_lapsed = end_time - start_time
+      time_convert(time_lapsed)
+      print(time_lapsed)
+      wpm()
+  else:
+      print("you fucked up")
+      start()
 
 start()
