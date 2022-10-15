7b1cc766232c0cb841cd3ce48f5c3c675fb71aba
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2022-10-15 19:43:39 +0200

    little tries

--- flaskv.py
+++ flaskv.py
@@ -6,6 +6,28 @@ app = Flask(__name__)
 
 word = []
 
+def wpm():
+    slenth = len(prompt)
+    numword = slenth / 4.7
+    wordspm = round((numword / (time_lapsed/60)), 2)
+    wordspm = str(wordspm)
+    print("Your WPM is: ", wordspm)
+
+
+def start():
+      #input("Press Enter to start")
+    start_time = time.time()
+    if input() == prompt:
+        end_time = time.time()
+        global time_lapsed
+        time_lapsed = end_time - start_time
+        tlr = round(time_lapsed, 2)
+        #print(f"It took you {str(tlr)} seconds to type the prompt")
+        wpm()
+    else:
+        return redirect('/test')
+
+
 @app.route('/', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
@@ -23,46 +45,24 @@ def gfg():
         return redirect('/test')
     return render_template('index.html')
         
-
-def start():
-  #input("Press Enter to start")
-  start_time = time.time()
-  if input() == prompt:
-      end_time = time.time()
-      global time_lapsed
-      time_lapsed = end_time - start_time
-      tlr = round(time_lapsed, 2)
-    #print(f"It took you {str(tlr)} seconds to type the prompt")
-      wpm()
-  else:
-      #print("you fucked up")
-      start()
-
-#creates list of words
-
-
-
 @app.route('/test', methods =["GET", "POST"])
 def test():
     if request.method == "POST":
         pass
     return render_template('speed.html', promptlen = promptlen, prompt = prompt)
-
-
-def wpm():
-    slenth = len(prompt)
-    numword = slenth / 4.7
-    wordspm = round((numword / (time_lapsed/60)), 2)
-    wordspm = str(wordspm)
-    print("Your WPM is: ", wordspm)
-
-
-#Starts the timer and calculate the elapsed time
-
+ 
+@app.route('/start', methods =["GET"])   
+def checkvar():  
+    while True:
+        if tstartp == 1: 
+            start()
+            break
+        if tstartp != 1:
+            tstartp = request.values.get("tstart")
+    return "success"
+        
 
 if __name__=='__main__':
     app.run(debug=True)
-    
-start()
 
 #Credit to @WhineyMonkey10 for being helpful but for also eating all my cookies
